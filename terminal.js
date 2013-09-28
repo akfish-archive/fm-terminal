94ab151d29817767beff7b4fbf24053e58f9e797
AKFish <akfish@gmail.com>
2013-09-29 05:30:36 +0800

    Index page

---
+++ terminal.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+    $('body').terminal(function(command, term) {
+        if (command == 'test') {
+            term.echo("you just typed 'test'");
+        } else {
+            term.echo('unknown command');
+        }
+    }, { prompt: '>', name: 'test' });
+});
