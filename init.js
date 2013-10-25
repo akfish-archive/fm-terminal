880585977f88ceabe55bf74a582d92a6ccb9fd4c
AKFish <akfish@gmail.com>
2013-10-25 18:02:15 +0800

    Remove GA code from panel.html

--- init.js
+++ init.js
@@ -4,15 +4,3 @@ var proxy_domain = "http://localhost:10080";
 var pipe_name = "catx.fm";
 console.log("Initial PipeName:" + pipe_name);
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-44221459-3']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-
-
